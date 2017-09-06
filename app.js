cff5c7447598f284d4c183fa4a09dd50c5c2741c
gazwald <gary.brandon@gmail.com>
2017-09-06 19:37:04 +1000

    Added ability to hide empty, full, and password protected servers. Added side menu for above options Added function processData to process above options

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var apiURL = 'http://192.168.124.84:5000'
+var apiURL = 'http://192.168.124.84:5000/'
 
 var shenanigans = new Vue({
 
@@ -9,39 +9,57 @@ var shenanigans = new Vue({
       { text: 'Hostname', value: 'hostname' },
       { text: 'Address', value: 'address' },
       { text: 'Map', value: 'map' },
-      { text: 'Protected', value: 'protected' },
-      { text: 'Players', value: 'players' },
-      { text: 'Max Players', value: 'maxplayers' }
+      { text: 'Password', value: 'needpass' },
+      { text: 'Players', value: 'clients' },
     ],
-    items: null,
-    showModal: false
+    data: null,
+    items: [],
+    currentGame: 'q2',
+    drawer: false,
+    hide_empty: false,
+    hide_full: false,
+    hide_protected: false
   },
 
   created: function () {
     this.fetchData()
   },
 
-  watch: {
-    currentBranch: 'fetchData'
-  },
-
-  filters: {
-    truncate: function (v) {
-      var newline = v.indexOf('\n')
-      return newline > 0 ? v.slice(0, newline) : v
-    },
+  beforeMount: function () {
+    this.processData()
   },
 
   methods: {
     fetchData: function () {
       var xhr = new XMLHttpRequest()
       var self = this
-      xhr.open('GET', apiURL)
+      xhr.open('GET', apiURL + self.currentGame + '/servers')
       xhr.onload = function () {
-        self.items = JSON.parse(xhr.responseText)
+        self.data = JSON.parse(xhr.responseText)
       }
       xhr.send()
+    },
+    processData: function () {
+      var self = this
+      console.log(self.data)
+      self.items = []
+      self.data.forEach(function(server)
+      {
+        show = true
+        if (self.hide_empty && server.clients == '0') { show = false; }
+        if (self.hide_full && server.clients >= server.maxclients ) { show = false; }
+        if (self.hide_protected && server.needpass != '0') { show = false; };
+        if (show) {
+          self.items.push(server)
+        }
+      });
     }
-  }
-})
+  },
 
+  watch: {
+    data: 'processData',
+    hide_empty: 'processData',
+    hide_full: 'processData',
+    hide_protected: 'processData',
+  },
+})
