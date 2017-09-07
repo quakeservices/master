c4b157ef2867ffb3da8c8589154687990a8a55e7
gazwald <gary.brandon@gmail.com>
2017-09-07 10:43:51 +1000

    Added ability to filter by gametype

--- app.js
+++ app.js
@@ -6,19 +6,23 @@ var shenanigans = new Vue({
 
   data: {
     headers: [
-      { text: 'Hostname', value: 'hostname' },
+      { text: 'Hostname',
+        value: 'hostname',
+        align: 'left' },
       { text: 'Address', value: 'address' },
       { text: 'Map', value: 'map' },
       { text: 'Password', value: 'needpass' },
       { text: 'Players', value: 'clients' },
     ],
-    data: null,
+    jsondata: null,
     items: [],
     currentGame: 'q2',
+    gameType: 'All',
+    gameTypes: null,
     drawer: false,
     hide_empty: false,
     hide_full: false,
-    hide_protected: false
+    hide_protected: false,
   },
 
   created: function () {
@@ -35,31 +39,35 @@ var shenanigans = new Vue({
       var self = this
       xhr.open('GET', apiURL + self.currentGame + '/servers')
       xhr.onload = function () {
-        self.data = JSON.parse(xhr.responseText)
+        self.jsondata = JSON.parse(xhr.responseText)
       }
       xhr.send()
     },
     processData: function () {
       var self = this
-      console.log(self.data)
       self.items = []
-      self.data.forEach(function(server)
+      self.gameTypes = []
+      self.jsondata.forEach(function(server)
       {
         show = true
         if (self.hide_empty && server.clients == '0') { show = false; }
         if (self.hide_full && server.clients >= server.maxclients ) { show = false; }
         if (self.hide_protected && server.needpass != '0') { show = false; };
+        if (server.gamename != self.gameType && self.gameType != 'All') { show = false };
         if (show) {
           self.items.push(server)
         }
       });
+      self.gameTypes = [...new Set(self.jsondata.map(item => item.gamename))];
+      self.gameTypes.unshift({text: 'All'})
     }
   },
 
   watch: {
-    data: 'processData',
+    jsondata: 'processData',
     hide_empty: 'processData',
     hide_full: 'processData',
     hide_protected: 'processData',
+    gameType: 'processData',
   },
 })
