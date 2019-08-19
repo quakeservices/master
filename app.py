6a51e8e94fc253b1873aa000fdf8c4c8608baf11
gazwald <gary.brandon@gmail.com>
2019-08-19 02:54:43 +0000

    Args for debug logging

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import logging
 import sys
+import argparse
 
 from headers import Headers
 from storage import Storage
@@ -10,11 +11,16 @@ from masterserver import MasterServer
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.core import patch_all
 
-
 #xray_recorder.configure(aws_xray_tracing_name='master')
 #plugins = ('ECSPlugin')
 #xray_recorder.configure(plugins=plugins)
 #patch_all()
+
+def gather_args():
+  parser = argparse.ArgumentParser(description='Master Server for idTech servers.')
+  parser.add_argument('--debug', dest='debug', action="store_true")
+  return parser.parse_args()
+
 
 def setup_logging(level='INFO'):
   log_level = getattr(logging, level.upper())
@@ -36,7 +42,12 @@ def main():
     pass
 
 if __name__ == '__main__':
-  setup_logging()
+  args = gather_args()
+
+  if args.debug:
+    setup_logging('DEBUG')
+  else:
+    setup_logging()
 
   logging.info(f"Starting master server.")
   main()
