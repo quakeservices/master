88c5f0a116fb8cc5609e25aa086ecdcc3e9a8981
gazwald <gary.brandon@gmail.com>
2019-09-30 03:00:39 +0000

    Splitting client and server handling into their own functions. Also trialing splitlines instead of split.

--- masterserver/__init__.py
+++ masterserver/__init__.py
@@ -13,39 +13,42 @@ class MasterServer:
     self.transport = transport
 
   def datagram_received(self, data, address):
-    # TODO: Assumes split on \n
     response = None
-    if b'\n' in data:
-      headers, *status = data.split(b'\n')
-      logging.debug(f"{__class__.__name__ } - Recieved {headers} from {address[0]}:{address[1]}")
-    else:
-      logging.debug(f"{__class__.__name__ } - Recieved {data} from {address[0]}:{address[1]}")
-      headers, status = data, None
+    logging.debug(f"{__class__.__name__ } - Recieved {data} from {address[0]}:{address[1]}")
+    headers, *status = data.splitlines()
 
     result = self.protocols.is_client(headers)
     if result:
-      logging.debug(f"{__class__.__name__ } - Header belongs to client")
-      response_header = result.get('resp', None)
-      response = self.create_response(response_header, self.storage.list_servers(result.get('game')))
+      response = self.handle_client(result)
     else:
       result = self.protocols.is_server(headers)
-      if result:
-        server = GameServer(address, status, result.get('encoding'))
-        logging.debug(f"{__class__.__name__ } - Header belongs to server")
-        if server.is_valid:
-          if self.storage.get_server(server):
-            self.storage.update_server(server)
-          else:
-            self.storage.create_server(server)
-
-        if result.get('active', True) is False:
-          self.storage.server_shutdown(server)
-
-        response = result.get('resp', None)
+      response = self.handle_server(result)
 
     if response:
       logging.debug(f"{__class__.__name__ } - Sending {response} to {address}")
       self.transport.sendto(response, address)
+
+  def handle_client(self):
+    logging.debug(f"{__class__.__name__ } - Header belongs to client")
+    response_header = result.get('resp', None)
+    server_list = self.storage.list_servers(result.get('game'))
+    response = self.create_response(response_header, server_list)
+    return response
+
+  def handle_server(self):
+    logging.debug(f"{__class__.__name__ } - Header belongs to server")
+    server = GameServer(address, status, result.get('encoding'))
+    if server.is_valid:
+      if self.storage.get_server(server):
+        self.storage.update_server(server)
+      else:
+        self.storage.create_server(server)
+
+    if result.get('active', True) is False:
+      self.storage.server_shutdown(server)
+
+    response = result.get('resp', None)
+    return response
 
   def create_response(self, header, response):
     # TODO: Clean up, add type conparison for if we ever need to return non-bytes (???)
