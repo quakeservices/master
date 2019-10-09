3ab0acda00f949729f513d64ff6a103421483da5
gazwald <gary.brandon@gmail.com>
2019-10-09 01:18:18 +0000

    Whitespace

--- app.py
+++ app.py
@@ -9,36 +9,36 @@ from transport import Transport
 from masterserver import MasterServer
 
 def setup_logging(level='INFO'):
-  logging.getLogger('boto3').propagate = False
-  logging.getLogger('botocore').propagate = False
+    logging.getLogger('boto3').propagate = False
+    logging.getLogger('botocore').propagate = False
 
-  log_level = getattr(logging, level.upper())
+    log_level = getattr(logging, level.upper())
 
-  logging.basicConfig(stream=sys.stdout,
-                      level=log_level,
-                      format='%(asctime)s %(levelname)s %(message)s',
-                      datefmt='%Y-%m-%d %H:%M:%S')
+    logging.basicConfig(stream=sys.stdout,
+                        level=log_level,
+                        format='%(asctime)s %(levelname)s %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
 
 
 def main():
-  protocols = Protocols()
-  storage = Storage()
-  master = MasterServer(storage, protocols)
-  transport = Transport(master)
+    protocols = Protocols()
+    storage = Storage()
+    master = MasterServer(storage, protocols)
+    transport = Transport(master)
 
-  try:
-    transport.loop.run_forever()
-  except:
-    pass
+    try:
+        transport.loop.run_forever()
+    except:
+        pass
 
 if __name__ == '__main__':
-  os.environ['STAGE'] = os.getenv('STAGE', 'PRODUCTION')
+    os.environ['STAGE'] = os.getenv('STAGE', 'PRODUCTION')
 
-  if os.getenv('STAGE') == 'TESTING':
-    setup_logging('DEBUG')
-  else:
-    setup_logging()
+    if os.getenv('STAGE') == 'TESTING':
+        setup_logging('DEBUG')
+    else:
+        setup_logging()
 
-  logging.info(f"Starting master server.")
-  main()
-  logging.info(f"Master server stopped.")
+    logging.info(f"Starting master server.")
+    main()
+    logging.info(f"Master server stopped.")
