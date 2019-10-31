62e519cf42a4f04d74a524f5d70d11f3f3d9e4aa
gazwald <gary.brandon@gmail.com>
2019-10-31 02:15:03 +0000

    Adding HealthCheck class for eventual HTTP based application checks

--- transport/__init__.py
+++ transport/__init__.py
@@ -8,11 +8,10 @@ import logging
 class Transport():
     def __init__(self, master):
         logging.debug(f"{self.__class__.__name__ } - Initialising transport")
-        self.master = master
         self.loop = asyncio.get_event_loop()
         self.signal()
-        self.v4_udp_transport, self.v4_udp_protocol = self.listener(ipv=4)
-        self.v6_udp_transport, self.v6_udp_protocol = self.listener(ipv=6)
+        self.v4_udp_transport, self.v4_udp_protocol = self.listener(master, ipv=4)
+        self.v6_udp_transport, self.v6_udp_protocol = self.listener(master, ipv=6)
 
     def signal(self):
         logging.debug(f"{self.__class__.__name__ } - Setting up signals")
@@ -21,17 +20,17 @@ class Transport():
             self.loop.add_signal_handler(getattr(signal, signame),
                                          functools.partial(self.shutdown, signame))
 
-    def listener(self, ipv=4):
+    def listener(self, server, ipv=4, port=27900):
         if ipv == 6:
             logging.debug(f"{self.__class__.__name__ } - Setting up IPv6 listener")
             socket_family = socket.AF_INET6
-            bind = ('::', 27900)
+            bind = ('::', port)
         else:
             logging.debug(f"{self.__class__.__name__ } - Setting up IPv4 listener")
             socket_family = socket.AF_INET
-            bind = ('0.0.0.0', 27900)
+            bind = ('0.0.0.0', port)
 
-        listen = self.loop.create_datagram_endpoint(lambda: self.master,
+        listen = self.loop.create_datagram_endpoint(lambda: server,
                                                     family=socket_family,
                                                     local_addr=bind)
 
@@ -42,10 +41,19 @@ class Transport():
     def shutdown(self, sig):
         logging.debug(f"{self.__class__.__name__ } - Caught {sig}")
         if self.v4_udp_transport:
-            logging.debug(f"{self.__class__.__name__ } - Closing IPv4 transport")
+            logging.debug(f"{self.__class__.__name__ } - Closing IPv4 UDP transport")
             self.v4_udp_transport.close()
 
         if self.v6_udp_transport:
-            logging.debug(f"{self.__class__.__name__ } - Closing IPv6 transport")
+            logging.debug(f"{self.__class__.__name__ } - Closing IPv6 UDP transport")
             self.v6_udp_transport.close()
         self.loop.stop()
+
+
+class HealthCheck(asyncio.Protocol):
+    def connection_made(self, transport):
+        self.transport = transport #pylint: disable=attribute-defined-outside-init
+
+    def data_received(self, data):
+        self.transport.write(b'HTTP/1.1 200 Success')
+        self.transport.close()
