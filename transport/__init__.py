d16e4114468c5dc45a5bffc34ddfbfdf002caf69
gazwald <gary.brandon@gmail.com>
2019-10-31 01:51:46 +0000

    Splitting listener up into IPv4 and IPv6

--- transport/__init__.py
+++ transport/__init__.py
@@ -7,12 +7,12 @@ import logging
 
 class Transport():
     def __init__(self, master):
-        logging.debug(f"{self.__class__.__name__ } - Initialising transport.")
-        self.bind = ('0.0.0.0', 27900)
+        logging.debug(f"{self.__class__.__name__ } - Initialising transport")
         self.master = master
         self.loop = asyncio.get_event_loop()
         self.signal()
-        self.listener()
+        self.v4_udp_transport, self.v4_udp_protocol = self.listener(ipv=4)
+        self.v6_udp_transport, self.v6_udp_protocol = self.listener(ipv=6)
 
     def signal(self):
         logging.debug(f"{self.__class__.__name__ } - Setting up signals")
@@ -21,16 +21,31 @@ class Transport():
             self.loop.add_signal_handler(getattr(signal, signame),
                                          functools.partial(self.shutdown, signame))
 
-    def listener(self):
-        logging.debug(f"{self.__class__.__name__ } - Setting up listener")
-        self.listen = self.loop.create_datagram_endpoint(lambda: self.master,
-                                                         family=socket.AF_INET6,
-                                                         flags=socket.AI_V4MAPPED,
-                                                         local_addr=self.bind)
+    def listener(self, ipv=4):
+        if ipv == 6:
+            logging.debug(f"{self.__class__.__name__ } - Setting up IPv6 listener")
+            socket_family = socket.AF_INET6
+            bind = ('::', 27900)
+        else:
+            logging.debug(f"{self.__class__.__name__ } - Setting up IPv4 listener")
+            socket_family = socket.AF_INET
+            bind = ('0.0.0.0', 27900)
 
-        self.transport, self.protocol = self.loop.run_until_complete(self.listen)
+        listen = self.loop.create_datagram_endpoint(lambda: self.master,
+                                                    family=socket_family,
+                                                    local_addr=bind)
+
+        transport, protocol = self.loop.run_until_complete(listen)
+
+        return transport, protocol
 
     def shutdown(self, sig):
         logging.debug(f"{self.__class__.__name__ } - Caught {sig}")
-        self.transport.close()
+        if self.v4_udp_transport:
+            logging.debug(f"{self.__class__.__name__ } - Closing IPv4 transport")
+            self.v4_udp_transport.close()
+
+        if self.v6_udp_transport:
+            logging.debug(f"{self.__class__.__name__ } - Closing IPv6 transport")
+            self.v6_udp_transport.close()
         self.loop.stop()
