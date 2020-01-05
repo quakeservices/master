30df10e8c366822c68a2356e1b9a6e3bac142600
gazwald <gary.brandon@gmail.com>
2020-01-05 13:57:10 +1100

    Removing environment check for the moment

--- __init__.py
+++ __init__.py
@@ -13,12 +13,9 @@ class Storage():
     def __init__(self):
         logging.debug(f"{self.__class__.__name__ } - Initialising storage.")
         self.cache = Cache()
-        if not os.getenv('SKIP_TABLE_CREATE', 'True'):
-            logging.debug(f"{self.__class__.__name__ } - Creating table...")
-            self.create_table()
-            logging.debug(f"{self.__class__.__name__ } - Table created.")
-        else:
-            logging.debug(f"{self.__class__.__name__ } - Skipping table creation.")
+        logging.debug(f"{self.__class__.__name__ } - Creating table...")
+        self.create_table()
+        logging.debug(f"{self.__class__.__name__ } - Table created.")
 
     @staticmethod
     def create_table():
