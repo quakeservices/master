bbf7e6920feef68f3c94451b6a737504abec562a
gazwald <gary.brandon@gmail.com>
2020-01-11 14:32:59 +1100

    Updated lambda to work with new storage methods

--- lambda.py
+++ lambda.py
@@ -1,12 +1,25 @@
+#!/usr/bin/env python
 import json
-from storage.model import Server
+from storage import Storage
 
-
-def list_servers():
-    return Server.scan()
 
 def lambda_handler(event, context):
+    storage = Storage()
+    server_list = list()
+    for server in storage.list_servers():
+        status = json.loads(server.status)
+        server_list.append(dict(address = server.address,
+                                hostname = status.get('hostname', 'unknown'),
+                                mapname = status.get('mapname', 'unknown'),
+                                players = server.player_count,
+                                maxplayers = status.get('maxclients', '-1'),
+                                password = status.get('needpass', '-1'),
+                                country = server.country_code))
+
     return {
         'statusCode': 200,
-        'body': json.dumps(list_servers())
+        'body': json.dumps(server_list)
     }
+
+if __name__ == '__main__':
+    print(lambda_handler('', ''))
