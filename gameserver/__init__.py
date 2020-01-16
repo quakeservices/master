63da61987eb4dcffc437327f7218eb6bebb2c3fe
gazwald <gary.brandon@gmail.com>
2020-01-16 12:46:43 +1100

    Not a docstring

--- gameserver/__init__.py
+++ gameserver/__init__.py
@@ -84,10 +84,8 @@ class GameServer():
                 self.players.append(player.groupdict())
 
     def dictify_status(self, data):
-        """
-        TODO: Investigate replacing this with Construct
-              At the very least look at struct
-        """
+        # TODO: Investigate replacing this with Construct
+        #       At the very least look at struct
 
         split_on = self.result.get('split_on', '\\')
 
