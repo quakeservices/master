a11315fde740c69e64090fe15ff1020d12d65b94
gazwald <gary.brandon@gmail.com>
2020-01-28 12:50:59 +1100

    Created server_info function

--- lambda.py
+++ lambda.py
@@ -6,8 +6,10 @@ HEADERS = {'Content-Type': 'application/json',
            'Access-Control-Allow-Origin': 'https://www.quake.services',
            'Vary': 'Origin'}
 
-def lambda_handler(event, context):
-    storage = Storage()
+200_RESPONSE = { 'statusCode': 200,
+                 'headers': HEADERS }
+
+def server_list(storage):
     server_list = list()
     for server in storage.list_servers():
         status = json.loads(server.status)
@@ -19,11 +21,32 @@ def lambda_handler(event, context):
                                 password = status.get('needpass', '-1'),
                                 country = server.country_code))
 
-    return {
-        'statusCode': 200,
-        'headers': HEADERS,
-        'body': json.dumps(server_list)
-    }
+    response = 200_RESPONSE
+    response['body'] = json.dumps(server_list)
+    return response
+
+def server_info(storage, server_id):
+    # get_server_obj expects a GameServer object.
+    # get_server only returns True/False
+    # TODO: Fix Storage module to work with the below use case
+    server_obj = storage.get_server_obj(server_id):
+
+    status = json.loads(server.status)
+    players = json.loads(server.players)
+    server_info = { 'status': status,
+                    'players': players}
+
+    response = 200_RESPONSE
+    response['body'] = json.dumps(server_info)
+    return response
+
+def lambda_handler(event, context):
+    # TODO: Add basic routing
+    # /servers = return all active servers
+    # /server/ = return all information about server
+    storage = Storage()
+    return server_list(storage)
+
 
 if __name__ == '__main__':
     print(lambda_handler('', ''))
