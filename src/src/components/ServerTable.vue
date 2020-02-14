1102208739e5aadca6adfb087c2d6deb2e9d9b84
gazwald <gary.brandon@gmail.com>
2020-02-14 12:32:41 +1100

    Moving server address into expander. Changing boolean from True/False to Yes/No for readability. Added gametype

--- src/src/components/ServerTable.vue
+++ src/src/components/ServerTable.vue
@@ -6,13 +6,18 @@
       :items="servers"
       :items-per-page="20"
       class="elevation-1"
+      show-expand
+      single-expand
     >
       <template v-slot:item.players="{ item }">
         {{ item.players }} / {{ item.maxplayers }}
       </template>
       <template v-slot:item.password="{ item }">
-        <template v-if="item.password == 1">True</template>
-        <template v-else>False</template>
+        <template v-if="item.password == 1">Yes</template>
+        <template v-else>No</template>
+      </template>
+      <template v-slot:expanded-item="{ item }">
+        <td :colspan="headers.length">{{ item.address }}</td>
       </template>
     </v-data-table>
   </div>
@@ -25,8 +30,8 @@
     name: 'server-table',
     data () {
       return {
-        headers: [ {text: 'Address', value: 'address'},
-                   {text: 'Hostname', value: 'hostname'},
+        headers: [ {text: 'Hostname', value: 'hostname'},
+                   {text: 'Mode', value: 'gametype'},
                    {text: 'Map', value: 'mapname'},
                    {text: 'Players', value: 'players'},
                    {text: 'Password', value: 'password'},
