9cce3fb0c22fdaac193f87ae11b422b183a994fa
gazwald <gary.brandon@gmail.com>
2020-03-11 12:47:21 +1100

    Type hinting for protocols

--- protocols/__init__.py
+++ protocols/__init__.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import yaml
+from typing import List, NoReturn
 
 from .proxy import ProxyProtocol
 
@@ -21,7 +22,7 @@ class Protocols():
         self.load_protocols()
 
     @staticmethod
-    def gather_protocol_files():
+    def gather_protocol_files() -> List[str]:
         config_files = []
         module_path = os.path.abspath(os.path.dirname(__file__))
         config_path = os.path.join(module_path, 'config')
@@ -31,12 +32,12 @@ class Protocols():
 
         return config_files
 
-    def load_protocols(self):
+    def load_protocols(self) -> NoReturn:
         logging.debug(f"{self.__class__.__name__ } - Loading protocols...")
         for config_file in self.gather_protocol_files():
             self.load_protocol(config_file)
 
-    def load_protocol(self, config_file):
+    def load_protocol(self, config_file: str) -> NoReturn:
         logging.debug(f"{self.__class__.__name__ } - Reading {config_file}")
         with open(config_file, 'rb') as config_file_handle:
             config = yaml.load(config_file_handle, Loader=yaml.FullLoader)
@@ -44,7 +45,7 @@ class Protocols():
                 self.protocols.append(GameProtocol(config))
                 logging.debug(f"{self.__class__.__name__ } - Loaded {config}")
 
-    def find_protocol(self, header):
+    def find_protocol(self, header: str):
         for game in self.protocols:
             result = game.match_header(self.header_order, header)
             if result:
@@ -80,18 +81,20 @@ class GameProtocol():
         logging.debug(f"{self.__class__.__name__ } - Initialising protocols for {self.name}")
         self.process_headers()
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.name
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self.protocol.get('game', 'Unknown')
 
     @property
-    def encoding(self):
+    def encoding(self) -> str:
         return self.protocol.get('encoding', 'latin1')
 
-    def match_header(self, header_order, header):
+    def match_header(self,
+                     header_order: str, 
+                     header: bytes):
         for header_class in [_ for _ in header_order if _ in self.protocol.keys()]:
             for k, v in self.protocol[header_class].items():  # pylint: disable=invalid-name
                 if v.get('recv', '').startswith(header):
@@ -103,7 +106,7 @@ class GameProtocol():
 
         return False
 
-    def process_headers(self):
+    def process_headers(self) -> NoReturn:
         for header_class in ['S2M', 'B2M']:
             for k, v in self.protocol[header_class].items():   # pylint: disable=invalid-name
                 self.protocol[header_class][k] = self.encode_headers(v)
@@ -114,5 +117,5 @@ class GameProtocol():
                 headers[_] = self.encoder(headers[_])
         return headers
 
-    def encoder(self, data):
+    def encoder(self, data: str) -> bytes:
         return bytes(data, self.encoding, errors='backslashreplace')
