cd6305bac6f72f0e2e66d54919047340b7fab721
gazwald <gary.brandon@gmail.com>
2021-06-30 19:26:45 +1000

    Formatting changes after black

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ from aws_cdk import core
 
 from deploy.deploy_stack import WebBackendDeployStack
 
-env = {'account': os.getenv('AWS_ACCOUNT', os.getenv('CDK_DEFAULT_ACCOUNT', '')),
-       'region': os.getenv('AWS_DEFAULT_REGION', os.getenv('CDK_DEFAULT_REGION', ''))}
+env = {
+    "account": os.getenv("AWS_ACCOUNT", os.getenv("CDK_DEFAULT_ACCOUNT", "")),
+    "region": os.getenv("AWS_DEFAULT_REGION", os.getenv("CDK_DEFAULT_REGION", "")),
+}
 
 
 app = core.App()
--- deploy/deploy_stack.py
+++ deploy/deploy_stack.py
@@ -2,74 +2,79 @@ import os
 import boto3
 
 from aws_cdk import (
-        core,
-        aws_certificatemanager as certificatemanager,
-        aws_apigateway as apigateway,
-        aws_lambda as _lambda,
-        aws_s3 as s3,
-        aws_iam as iam,
-        aws_route53 as route53,
-        aws_route53_targets as route53_targets
+    core,
+    aws_certificatemanager as certificatemanager,
+    aws_apigateway as apigateway,
+    aws_lambda as _lambda,
+    aws_s3 as s3,
+    aws_iam as iam,
+    aws_route53 as route53,
+    aws_route53_targets as route53_targets,
 )
 
 
 class WebBackendDeployStack(core.Stack):
-
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        self.zone = route53.HostedZone.from_lookup(self, "quake_services", domain_name="quake.services")
+        self.zone = route53.HostedZone.from_lookup(
+            self, "quake_services", domain_name="quake.services"
+        )
 
         """
         Get existing certificate
         """
-        arn = "arn:aws:acm:{region}:{account}:certificate/{cert}".format(region='us-west-2',
-                                                                          account=os.getenv("CDK_DEFAULT_ACCOUNT"),
-                                                                          cert='6744abde-0b71-4aa5-94b1-a9f554fb1116')
-        certificate = certificatemanager.Certificate.from_certificate_arn(self,
-                                                                          "wildcard_cert",
-                                                                          arn)
-
+        arn = "arn:aws:acm:{region}:{account}:certificate/{cert}".format(
+            region="us-west-2",
+            account=os.getenv("CDK_DEFAULT_ACCOUNT"),
+            cert="6744abde-0b71-4aa5-94b1-a9f554fb1116",
+        )
+        certificate = certificatemanager.Certificate.from_certificate_arn(
+            self, "wildcard_cert", arn
+        )
 
         policy = iam.PolicyStatement(
             resources=["*"],
-            actions=["dynamodb:Get*",
-                     "dynamodb:Query",
-                     "dynamodb:Scan",
-                     "dynamodb:Describe*",
-                     "dynamodb:List*"])
-
+            actions=[
+                "dynamodb:Get*",
+                "dynamodb:Query",
+                "dynamodb:Scan",
+                "dynamodb:Describe*",
+                "dynamodb:List*",
+            ],
+        )
 
         """
         Define domain name and certificate to use for API Gateway
         """
-        domain = {'domain_name': 'api.quake.services',
-                  'certificate': certificate}
+        domain = {"domain_name": "api.quake.services", "certificate": certificate}
 
         """
         Get latest version of code
         There is probably a more elegant way of doing this
         But for now this works
         """
-        s3_bucket_name = 'web-backend-lambda-package'
-        s3client = boto3.client('s3')
-        latest_version = s3client.get_object_tagging(Bucket=s3_bucket_name,
-                                                     Key='function.zip')
-        bucket = s3.Bucket.from_bucket_name(self,
-                                            'bucket',
-                                            bucket_name=s3_bucket_name)
+        s3_bucket_name = "web-backend-lambda-package"
+        s3client = boto3.client("s3")
+        latest_version = s3client.get_object_tagging(
+            Bucket=s3_bucket_name, Key="function.zip"
+        )
+        bucket = s3.Bucket.from_bucket_name(self, "bucket", bucket_name=s3_bucket_name)
 
         """
         Define Lambda function
         """
-        code = _lambda.Code.from_bucket(bucket=bucket,
-                                        key='function.zip',
-                                        object_version=latest_version.get('VersionId'))
+        code = _lambda.Code.from_bucket(
+            bucket=bucket,
+            key="function.zip",
+            object_version=latest_version.get("VersionId"),
+        )
 
         backend = _lambda.Function(
-            self, 'web-backend',
+            self,
+            "web-backend",
             runtime=_lambda.Runtime.PYTHON_3_8,
-            handler='lambda.lambda_handler',
+            handler="lambda.lambda_handler",
             code=code,
         )
 
@@ -78,20 +83,25 @@ class WebBackendDeployStack(core.Stack):
         """
         Define API Gateway
         """
-        api = apigateway.LambdaRestApi(self,
-                                       "QuakeServicesAPI",
-                                       domain_name=domain,
-                                       handler=backend,
-                                       default_cors_preflight_options={
-                                           "allow_origins": ['www.quake.services'],
-                                           "allow_methods": ['GET']
-                                       })
+        api = apigateway.LambdaRestApi(
+            self,
+            "QuakeServicesAPI",
+            domain_name=domain,
+            handler=backend,
+            default_cors_preflight_options={
+                "allow_origins": ["www.quake.services"],
+                "allow_methods": ["GET"],
+            },
+        )
         """
         Create Route53 entries
         """
-        route53.ARecord(self, "Alias",
+        route53.ARecord(
+            self,
+            "Alias",
             zone=self.zone,
-            record_name='api',
-            target=route53.AddressRecordTarget.from_alias(route53_targets.ApiGateway(api))
+            record_name="api",
+            target=route53.AddressRecordTarget.from_alias(
+                route53_targets.ApiGateway(api)
+            ),
         )
-
