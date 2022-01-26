a30d76b67f371315bb6211a0c4bc3589c7d4e280
gazwald <gary.brandon@gmail.com>
2022-01-26 15:52:45 +1100

    Cleaning up deployment dir

--- deployment/app.py
+++ deployment/app.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 import os
-import boto3
 
 from aws_cdk import core as cdk
 
-from master_deploy.master_deploy_stack import MasterDeployStack
+from stacks.master import MasterDeployStack
+from stacks.web_backend import WebBackendDeployStack
 
 env = {
     "account": os.getenv("AWS_ACCOUNT", os.getenv("CDK_DEFAULT_ACCOUNT", "")),
     "region": os.getenv("AWS_DEFAULT_REGION", os.getenv("CDK_DEFAULT_REGION", "")),
 }
 
+
 app = cdk.App()
 
-MasterDeployStack(app, "master-deploy", env=env)
+MasterDeployStack(app, "master", env=env)
+WebBackendDeployStack(app, "master-web-backend", env=env)
 
 app.synth()
