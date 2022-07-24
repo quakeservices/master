3948abd6cf4931f97d096595f7fc8872745911ac
gazwald <gary.brandon@gmail.com>
2022-07-25 08:08:43 +1000

    Unused stacks

--- cdk.py
+++ cdk.py
@@ -8,9 +8,5 @@ from deployment.stacks.pipeline import PipelineStack
 app = App()
 
 PipelineStack(app, f"{APP_NAME}-pipeline", env=us_west_2)
-# InfraStack(app, f"{APP_NAME}-infra", env=us_west_2)
-# MasterStack(app, f"{APP_NAME}-master", env=us_west_2)
-# WebBackendStack(app, f"{APP_NAME}-backend", env=us_west_2)
-# WebFrontendStack(app, f"{APP_NAME}-frontend", env=us_east_1)
 
 app.synth()
