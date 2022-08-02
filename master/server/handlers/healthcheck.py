c07c6303dec931ea1b0c423f03952467ff2f3d4e
gazwald <gary.brandon@gmail.com>
2022-08-03 08:29:48 +1000

    Fixed healthcheck

--- master/server/handlers/healthcheck.py
+++ master/server/handlers/healthcheck.py
@@ -1,11 +1,19 @@
 import logging
 from socketserver import StreamRequestHandler
 
+PROTOCOL: str = "HTTP/1.1"
+STATUS: str = "200"
+STATUS_TEXT: str = "OK"
+NEWLINE: str = "\r\n"
+RESPONSE_TEXT: str = "Success!"
+RESPONSE: bytes = (
+    f"{PROTOCOL} {STATUS} {STATUS_TEXT}{NEWLINE}{NEWLINE}{RESPONSE_TEXT}".encode(
+        "utf-8"
+    )
+)
+
 
 class HealthCheckHandler(StreamRequestHandler):
-    def handler(self) -> None:
+    def handle(self) -> None:
         logging.debug("Received health check ping")
-        response: bytes = (
-            b"HTTP/1.1 200 OK\nContent-Type: text/html\n<html><body>OK</body></html>\n"
-        )
-        self.wfile.write(response)
+        self.wfile.write(RESPONSE)
