2b70cbc5c8a8d856c82671e087dd81dc3eff0afa
gazwald <gary.brandon@gmail.com>
2022-08-20 08:22:44 +1000

    Option to exclude time from logs

--- master/helpers/__init__.py
+++ master/helpers/__init__.py
@@ -3,15 +3,22 @@ import os
 import sys
 
 
-def setup_logging(log_level: str, hide_boto_logs: bool) -> None:
+def setup_logging(
+    log_level: str, hide_boto_logs: bool, show_time: bool = False
+) -> None:
     if hide_boto_logs:
         logging.getLogger("boto3").propagate = False
         logging.getLogger("botocore").propagate = False
 
+    if show_time:
+        format = "%(asctime)s %(levelname)s %(message)s"
+    else:
+        format = "%(levelname)s %(message)s"
+
     logging.basicConfig(
         stream=sys.stdout,
         level=getattr(logging, log_level.upper()),
-        format="%(asctime)s %(levelname)s %(message)s",
+        format=format,
         datefmt="%Y-%m-%d %H:%M:%S",
     )
 
