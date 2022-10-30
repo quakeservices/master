96b603ad2910737d8826f8bcc4df61eb1e8960e9
gazwald <gary.brandon@gmail.com>
2022-10-30 20:28:29 +1100

    Tests for master handler

--- master/server/handlers/master.py
+++ master/server/handlers/master.py
@@ -2,7 +2,6 @@ import logging
 import struct
 from ipaddress import ip_address
 from socketserver import DatagramRequestHandler
-from typing import Optional
 
 from master.protocols import ProtocolResponse, Protocols
 from master.storage import storage
@@ -11,15 +10,20 @@ from master.storage.models.server import Server
 
 class MasterHandler(DatagramRequestHandler):
     def handle(self) -> None:
-        protocols = Protocols()
         storage_class = storage(backend="dynamodb")
         if storage_class:
             self.storage = storage_class()  # pylint: disable=not-callable
 
+        request: bytes = self.__get_request()
+        if request:
+            self.__handle_request(request)
+
+    def __get_request(self) -> bytes:
         request: bytes = b""
+        fragment: bytes = b""
         while True:
-            fragment: bytes = self.rfile.readline()
-            if fragment != b"":
+            fragment = self.rfile.readline()
+            if fragment:
                 logging.debug(
                     "Recieved fragment %s from %s", fragment, self.client_address
                 )
@@ -29,18 +33,24 @@ class MasterHandler(DatagramRequestHandler):
 
         logging.info("Recieved %s from %s", request, self.client_address)
 
-        protocol_response: Optional[ProtocolResponse] = protocols.parse_request(request)
-        if protocol_response:
-            response: Optional[bytes] = None
-            if protocol_response.request_type == "client":
-                response = self._handle_client_request(protocol_response)
-            elif protocol_response.request_type == "server":
-                response = self._handle_server_request(protocol_response)
-            elif protocol_response.request_type == "any":
-                response = self._handle_generic_request(protocol_response)
+        return request
 
-            if response:
-                self._send_response(response)
+    def __handle_request(self, request: bytes) -> None:
+        protocols = Protocols()
+        protocol_response: ProtocolResponse | None = protocols.parse_request(request)
+        response: bytes | None
+        match protocol_response.request_type:
+            case "client":
+                response = self._handle_client_request(protocol_response)
+            case "server":
+                response = self._handle_server_request(protocol_response)
+            case "any":
+                response = self._handle_generic_request(protocol_response)
+            case _:
+                response = None
+
+        if response:
+            self._send_response(response)
 
     def _send_response(self, response: bytes) -> None:
         logging.debug("Sending %s to %s", response, self.client_address)
@@ -49,46 +59,44 @@ class MasterHandler(DatagramRequestHandler):
     def _handle_client_request(self, request: ProtocolResponse) -> bytes:
         logging.debug("Header belongs to client")
 
-        server_list: list[Server] = self.storage.get_servers(request.game)
         processed_server_list: list[bytes] = [
-            self._pack_address(server.address) for server in server_list
+            self._pack_address(server.address)
+            for server in self.storage.get_servers(request.game)
         ]
 
         return self._create_response(processed_server_list, request.response)
 
-    def _handle_server_request(self, request: ProtocolResponse) -> Optional[bytes]:
+    def _handle_server_request(self, request: ProtocolResponse) -> bytes | None:
         logging.debug("Header belongs to server")
-        address = ":".join([self.client_address[0], str(self.client_address[1])])
-        server = Server(
-            address=address,
-            game=request.game,
-            active=request.active,
-            details=request.details,
-            players=request.players,
+        address, port = self.client_address
+        self.storage.update_server(
+            Server(
+                address=":".join([address, str(port)]),
+                game=request.game,
+                active=request.active,
+                details=request.details,
+                players=request.players,
+            )
         )
-
-        self.storage.update_server(server)
 
         return request.response
 
-    def _handle_generic_request(self, request: ProtocolResponse) -> Optional[bytes]:
+    @staticmethod
+    def _handle_generic_request(request: ProtocolResponse) -> bytes | None:
         logging.debug("Header could belong to anything")
-        if request.response:
-            return request.response
-
-        return None
+        return request.response
 
     @staticmethod
     def _create_response(
         response: list[bytes],
-        header: Optional[bytes] = None,
-        seperator: bytes = b"",
+        header: bytes | None = None,
+        separator: bytes = b"",
     ) -> bytes:
 
         if header:
-            response.insert(0, header)
+            response[0] = header + response[0]
 
-        return seperator.join(response)
+        return separator.join(response)
 
     @staticmethod
     def _pack_address(address: str, port_format: str = ">H") -> bytes:
